db5d3b136be4aaa2108e569ae127dcb878da7bdc
Leonardo Calderon <leov.calderon@gmail.com>
2018-01-20 12:19:49 -0500

    nivel basico video 8

--- src/app/countdown/countdown.component.ts
+++ src/app/countdown/countdown.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-countdown',
   templateUrl: './countdown.component.html',
   styleUrls: ['./countdown.component.scss']
 })
-export class CountdownComponent{
+export class CountdownComponent implements OnInit {
+  
+  
+
+  @Input() init:number = null;
+  public counter:number = 0;
+
+  ngOnInit(): void {
+    this.startCountDown()
+  }
 
   constructor() { }
+
+
+  startCountDown() {
+    if (this.init && this.init > 0) {
+      this.counter = this.init;
+      this.doCountDown();
+    }
+  }
+
+  doCountDown() {
+    setTimeout(() => {
+      this.counter = this.counter - 1;
+      this.processCountDown();
+
+    }, 1000)
+  }
+
+  processCountDown() {
+    //emite evento
+    console.log("count is: ", this.counter);
+
+    if(this.counter == 0) {
+      console.log("counter END");
+    } else {
+      this.doCountDown();
+    }
+  }
 
 }
